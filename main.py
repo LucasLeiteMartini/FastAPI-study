ce27ec671fca43b8fb847fa46a8b551549d63f1e
Lucas Martini <lucas.leite.martini@gmail.com>
2025-05-13 11:07:30 -0400

    teste

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from fastapi import FastAPI
 
+from enum import Enum
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+
 app = FastAPI()
 
 
@@ -11,3 +20,30 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update({
+            "description": "This is an amazing item that has a long description",
+        })
+    return item
+
+
+@app.get("/models/{model_name}")
+def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {model_name: model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name.value, "message": "Have some residuals"}
+
+@app.get("/files/{file_path:path}")
+def read_file(file_path: str):
+    return {"file_path": file_path}
+
